69c5d648217ac36ef9910909b63cb6281e029c8b
Rafael Fernando <rafaelribeirosouza86@gmail.com>
2024-02-17 11:46:57 -0300

    Added Stream Interceptor Bearer and  Unary Interceptor Bearer

--- auth-jwt.go
+++ auth-jwt.go
@@ -1,13 +1,15 @@
 package authjwt
 
 import (
+	"context"
 	"errors"
-	"fmt"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 )
 
@@ -18,11 +20,11 @@ type Credential struct {
 
 func NewCredential(Expire int, SecretKey string) (*Credential, error) {
 	if Expire <= 0 {
-		return nil, errors.New("o tempo de expiração deve ser maior que zero")
+		return nil, errors.New("expiration time must be greater than zero")
 	}
 
 	if SecretKey == "" {
-		return nil, errors.New("a chave secreta não pode estar vazia")
+		return nil, errors.New("the secret key cannot be empty")
 	}
 
 	c := &Credential{
@@ -58,7 +60,7 @@ func (c *Credential) VerifyToken(tokenString string) error {
 	}
 
 	if !token.Valid {
-		return fmt.Errorf("invalid token")
+		return status.Error(codes.Unauthenticated, "Token inválido")
 	}
 
 	return nil
@@ -74,6 +76,7 @@ func (c *Credential) TokenExpired(tokenString string) bool {
 // jwtStreamInterceptor é o interceptor gRPC para autenticação JWT em streams
 func (c *Credential) JwtStreamInterceptor(token string) func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+
 		err := c.VerifyToken(token)
 
 		if err != nil {
@@ -87,4 +90,77 @@ func (c *Credential) JwtStreamInterceptor(token string) func(srv interface{}, ss
 		// Se não houver erro, chame o manipulador de chamada de streaming
 		return handler(srv, ss)
 	}
+}
+
+// JwtUnaryInterceptor é o interceptor gRPC para autenticação JWT em unary
+func (c *Credential) JwtUnaryInterceptor(token string) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		err := c.VerifyToken(token)
+
+		if err != nil {
+			return nil, status.Error(codes.Unauthenticated, "Token inválido: "+err.Error())
+		}
+
+		if c.TokenExpired(token) {
+			return nil, status.Error(codes.Unauthenticated, "Token Expirou")
+		}
+
+		// If no errors, proceed with the unary handler
+		return handler(ctx, req)
+	}
+}
+
+func (i *Credential) StreamInterceptorBearer(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	token, err := GetToken(ss.Context())
+
+	if err != nil {
+		return status.Errorf(codes.Unauthenticated, "invalid token: %v", err)
+	}
+
+	err = i.VerifyToken(token)
+
+	if err != nil {
+		return status.Errorf(codes.Unauthenticated, "invalid token: %v", err)
+	}
+
+	return handler(srv, ss)
+}
+
+func (i *Credential) UnaryInterceptorBearer(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+
+	token, err := GetToken(ctx)
+
+	if err != nil {
+		return nil, status.Errorf(codes.Unauthenticated, "invalid token: %v", err)
+	}
+
+	err = i.VerifyToken(token)
+
+	if err != nil {
+		return nil, status.Errorf(codes.Unauthenticated, "invalid token: %v", err)
+	}
+
+	// Continue with the handler if authentication is successful
+	return handler(ctx, req)
+}
+
+func GetToken(ctx context.Context) (string, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+
+	if !ok {
+		return "", status.Errorf(codes.Unauthenticated, "missing metadata")
+	}
+
+	authHeader, ok := md["authorization"]
+	if !ok || len(authHeader) == 0 {
+		return "", status.Errorf(codes.Unauthenticated, "missing authorization header")
+	}
+
+	parts := strings.Split(authHeader[0], " ")
+	if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
+		return "", status.Errorf(codes.Unauthenticated, "invalid authorization format")
+	}
+
+	return parts[1], nil
+
 }
